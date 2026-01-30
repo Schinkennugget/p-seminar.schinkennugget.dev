6a7f12b43b70f3c922fb8d84156913fc79a7f334
Schinkennugget <jabo.dichtl@icloud.com>
2026-01-30 10:36:31 +0100

    made daten-abschnitt foldable

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,11 +1,44 @@
+"use strict";
+
 const media = window.matchMedia('(prefers-color-scheme: dark)');
 let darkModeEnabled = false;
+
+// Alle Farben
+// const backgroundDark = "#fff";
+// const backgroundDark = "#111";
+// const accent1TransparentLight = "rgba(248, 248, 255, 0.75)";
+// const accent1TransparentDark = "rgba(50, 50, 50, 0.75)";
+// const accent1Light = "#f8f8ff";
+// const accent1Dark = "#222";
+// const accent2Light = "#f5f5f6";
+// const accent2Dark = "#333";
+// const accent3Light = "#ededef";
+// const accent3Dark = "#444";
+// const accent4Light = "#e0e0e2";
+// const accent4Dark = "#555";
+// const accent5Light = "#444";
+// const accent5Dark = "#666";
+// const textColorLight = "#000";
+// const textColorDark = "#fff";
+// const linkColorLight = "#11a";
+// const linkColorDark = "#66f";
+
+let datenShown = false;
 
 function toggleDarkMode() {
 	const r = document.documentElement;
 	darkModeEnabled = darkModeEnabled ? false : true;
 
 	if (darkModeEnabled) {
+		// r.style.setProperty('--background', backgroundDark);
+		// r.style.setProperty('--accent1-transparent', accent1TransparentLight);
+		// r.style.setProperty('--accent1', accent1Dark);
+		// r.style.setProperty('--accent2', accent2Dark);
+		// r.style.setProperty('--accent3', accent3Dark);
+		// r.style.setProperty('--accent4', accent4Dark);
+		// r.style.setProperty('--accent5', accent5Dark);
+		// r.style.setProperty('--text', textColorDark);
+		// r.style.setProperty('--link-color', linkColorDark);
 		r.style.setProperty('--background', '#111');
 		r.style.setProperty('--accent1-transparent', 'rgba(50, 50, 50, 0.75)');
 		r.style.setProperty('--accent1', '#222');
@@ -29,7 +62,7 @@ function toggleDarkMode() {
 }
 
 // initial
-updateDarkMode(media.matches);
+toggleDarkMode(media.matches);
 
 // live changes
 if (media.addEventListener) {
@@ -37,3 +70,18 @@ if (media.addEventListener) {
 } else {
 	media.addListener(e => updateDarkMode(e.matches));
 }
+
+function toggleDaten() {
+	if (datenShown) {
+		datenShown = false;
+		document.getElementById("daten-werte-abschnitt").style.display = "none";
+		document.getElementById("daten-header-icon").style.rotate = "0deg"
+		r.style.setProperty('--daten-hover-background', r.style.getProperty('--accent1'));
+	} else {
+		datenShown = true;
+		document.getElementById("daten-werte-abschnitt").style.display = "grid";
+		document.getElementById("daten-header-icon").style.rotate = "90deg"
+	}
+}
+
+//Variable für die Höhe vom Daten-Abschnitt
