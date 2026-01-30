147d4845c3e644bdb5f0666f6eaa5bf53ce44ccb
Schinkennugget <jabo.dichtl@icloud.com>
2026-01-30 20:12:18 +0100

    animated daten toggle, show only sun or moon icon

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -2,6 +2,7 @@
 
 const media = window.matchMedia('(prefers-color-scheme: dark)');
 let darkModeEnabled = false;
+const cssRoot = document.documentElement;
 
 // Alle Farben
 // const backgroundDark = "#fff";
@@ -26,7 +27,6 @@ let darkModeEnabled = false;
 let datenShown = false;
 
 function toggleDarkMode() {
-	const r = document.documentElement;
 	darkModeEnabled = darkModeEnabled ? false : true;
 
 	if (darkModeEnabled) {
@@ -39,49 +39,86 @@ function toggleDarkMode() {
 		// r.style.setProperty('--accent5', accent5Dark);
 		// r.style.setProperty('--text', textColorDark);
 		// r.style.setProperty('--link-color', linkColorDark);
-		r.style.setProperty('--background', '#111');
-		r.style.setProperty('--accent1-transparent', 'rgba(50, 50, 50, 0.75)');
-		r.style.setProperty('--accent1', '#222');
-		r.style.setProperty('--accent2', '#333');
-		r.style.setProperty('--accent3', '#444');
-		r.style.setProperty('--accent4', '#555');
-		r.style.setProperty('--accent5', '#666');
-		r.style.setProperty('--text', '#fff');
-		r.style.setProperty('--link-color', '#66f');
+		cssRoot.style.setProperty('--background', '#111');
+		cssRoot.style.setProperty('--accent1-transparent', 'rgba(50, 50, 50, 0.75)');
+		cssRoot.style.setProperty('--accent1', '#222');
+		cssRoot.style.setProperty('--accent2', '#333');
+		cssRoot.style.setProperty('--accent3', '#444');
+		cssRoot.style.setProperty('--accent4', '#555');
+		cssRoot.style.setProperty('--accent5', '#666');
+		cssRoot.style.setProperty('--text', '#fff');
+		cssRoot.style.setProperty('--link-color', '#66f');
+		document.getElementById("navbar-icon-moon").style.display = "none";
+		document.getElementById("navbar-icon-sun").style.display = "inline";
 	} else {
-		r.style.setProperty('--background', '#fff');
-		r.style.setProperty('--accent1-transparent', 'rgba(248, 248, 255, 0.75)');
-		r.style.setProperty('--accent1', '#f8f8ff');
-		r.style.setProperty('--accent2', '#f5f5f6');
-		r.style.setProperty('--accent3', '#ededef');
-		r.style.setProperty('--accent4', '#e0e0e2');
-		r.style.setProperty('--accent5', '#444');
-		r.style.setProperty('--text', '#000');
-		r.style.setProperty('--link-color', '#11a');
+		cssRoot.style.setProperty('--background', '#fff');
+		cssRoot.style.setProperty('--accent1-transparent', 'rgba(248, 248, 255, 0.75)');
+		cssRoot.style.setProperty('--accent1', '#f8f8ff');
+		cssRoot.style.setProperty('--accent2', '#f5f5f6');
+		cssRoot.style.setProperty('--accent3', '#ededef');
+		cssRoot.style.setProperty('--accent4', '#e0e0e2');
+		cssRoot.style.setProperty('--accent5', '#444');
+		cssRoot.style.setProperty('--text', '#000');
+		cssRoot.style.setProperty('--link-color', '#11a');
+		document.getElementById("navbar-icon-sun").style.display = "none";
+		document.getElementById("navbar-icon-moon").style.display = "inline";
 	}
 }
 
-// initial
+// initialisierung dark mode
 toggleDarkMode(media.matches);
 
-// live changes
+// live changes vom dark mode
 if (media.addEventListener) {
 	media.addEventListener('change', e => updateDarkMode(e.matches));
 } else {
 	media.addListener(e => updateDarkMode(e.matches));
 }
 
+
+
+
+
+
+
+
+
 function toggleDaten() {
+	datenShown = datenShown ? false : true;
+	const datenWerteAbschnitt = document.getElementById("daten-werte-abschnitt");
+
 	if (datenShown) {
-		datenShown = false;
-		document.getElementById("daten-werte-abschnitt").style.display = "none";
-		document.getElementById("daten-header-icon").style.rotate = "0deg"
-		r.style.setProperty('--daten-hover-background', r.style.getProperty('--accent1'));
+		//Finde die Höhe, die es ausgeklappt hätte, setze die Höhe auf 0, zeig das
+		//Element, setze die Höhe auf die vorher bestimmte Höhe, setze nach Verzögerung
+		//für Animation die Höhe auf auto (damit Bilschirm resized werden kann)
+
+		datenWerteAbschnitt.style.transition = "none";
+		datenWerteAbschnitt.style.display = "grid";
+		datenWerteAbschnitt.style.height = "auto";
+		const datenHeight = datenWerteAbschnitt.offsetHeight;
+		// console.log(datenHeight + "px");
+		datenWerteAbschnitt.style.height = "0";
+		datenWerteAbschnitt.style.transition = "height 0.3s, transform 0.3s";
+
+		document.getElementById("daten-header-icon").style.rotate = "90deg";
+		datenWerteAbschnitt.style.height = datenHeight + "px";
+		datenWerteAbschnitt.style.transform = "scaleY(1)";
+
+		// setTimeout(function() {
+		datenWerteAbschnitt.style.height = "auto";
+		// }, 500);
+
+
 	} else {
-		datenShown = true;
-		document.getElementById("daten-werte-abschnitt").style.display = "grid";
-		document.getElementById("daten-header-icon").style.rotate = "90deg"
+		
+		datenWerteAbschnitt.style.transition = "height 0.3s, transform 0.15s";
+		datenWerteAbschnitt.style.transform = "scaleY(0.1)";
+		document.getElementById("daten-header-icon").style.rotate = "0deg";
+
+
+		setTimeout(function() {
+			datenWerteAbschnitt.style.display = "none";
+		}, 100);
+
 	}
 }
-
-//Variable für die Höhe vom Daten-Abschnitt
